0e941f1f84ef5edd3f53f5ae991ced006f28d22b
Zhitnyak Maksim <m.zhitnyak@gmail.com>
2022-01-13 20:50:38 +0200

    fixed fild

--- src/components/Forma/Forma.js
+++ src/components/Forma/Forma.js
@@ -41,6 +41,7 @@ function Form({ onSubmit }) {
           value={name}
           pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
           title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+          placeholder="Freddie Mercury"
           required
           onChange={handleChange}
         />
@@ -53,6 +54,7 @@ function Form({ onSubmit }) {
           value={number}
           pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
           title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+          placeholder="+38 (0XX) XXX-XX-XX"
           required
           onChange={handleChange}
         />
